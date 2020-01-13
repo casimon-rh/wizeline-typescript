4b110e97d766406ed17817b6de6ea6798f1badd8
Carlos Eduardo Simon Zepeda <casimon@redhat.com>
2020-01-13 15:34:34 -0600

    wizeline

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,31 @@
-type MapFn = <T>(items: T[], callback: (n: T) => T) => T[]
+import express, { Request, Response } from 'express'
+import bodyParser from 'body-parser'
+import * as fs from 'fs'
+import soap from 'soap'
 
-const m: MapFn = (items, cback) =>
-  items.map(e => cback(e))
+const app = express()
+app.set('port', process.env.PORT || 3000)
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-console.log(m([1, 2, 3], (n) => n ** 2))
-console.log(m(['1', '2', '3'], (n) => `-${n}-`))
+// app.all('*', (req: Request, res: Response): Response => {
+//   console.log('Not Found')
+//   return res.sendStatus(404)
+// })
+
+const serv = {
+  Serv: {
+    PortType: {
+      Func: (a: string) => ({p: a})
+    }
+  }
+}
+
+const xml = fs.readFileSync('../file.wsdl', 'utf-8')
+app.listen(app.get('port'), () => {
+  soap.listen(app, '/app', serv, xml, () => {
+    console.log('App running')
+  })
+})
+
+export default app
